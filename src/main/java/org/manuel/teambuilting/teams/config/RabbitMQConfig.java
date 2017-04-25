9f343893271d6c57a538e7732aa5e42ee557e0a7
Manuel Doncel Martos <manuel.doncel.martos@powerhouse.nl>
2017-04-25 15:42:13 +0200

    Removing a todo

--- src/main/java/org/manuel/teambuilting/teams/config/RabbitMQConfig.java
+++ src/main/java/org/manuel/teambuilting/teams/config/RabbitMQConfig.java
@@ -2,12 +2,18 @@ package org.manuel.teambuilting.teams.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Manuel Doncel Martos
@@ -18,7 +24,24 @@ public class RabbitMQConfig  {
 
     private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd\'T\'HH:mm:ss.SSSZ";
 
-    // TODO declare the echange
+	private final String exchange;
+	private final boolean durable;
+	private final boolean autodelete;
+	private final AmqpAdmin rabbitAdmin;
+
+	public RabbitMQConfig(final @Value("${messaging.amqp.team.exchange.name}") String exchange, final @Value("${messaging.amqp.team.exchange.durable}") boolean durable,
+		final @Value("${messaging.amqp.team.exchange.autodelete}") boolean autodelete, final AmqpAdmin rabbitAdmin) {
+    	this.exchange = exchange;
+    	this.durable = durable;
+    	this.autodelete = autodelete;
+		this.rabbitAdmin = rabbitAdmin;
+
+	}
+
+    @PostConstruct
+    private void declareExchange() {
+        rabbitAdmin.declareExchange(new TopicExchange(exchange, durable, autodelete));
+    }
 
     @Bean(name = "eventMessageConverter")
     public MessageConverter messageConverter() {
