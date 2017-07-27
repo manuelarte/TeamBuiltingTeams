61d34b48517c4cd9465c8bc46216633c24b26606
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-07-27 22:38:49 +0200

    Shared repos

--- src/main/java/org/manuel/teambuilting/teams/TeamBuiltingTeamsApplication.java
+++ src/main/java/org/manuel/teambuilting/teams/TeamBuiltingTeamsApplication.java
@@ -1,11 +1,14 @@
 package org.manuel.teambuilting.teams;
 
+import org.manuel.teambuilting.core.config.CoreConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@Import({CoreConfig.class})
 public class TeamBuiltingTeamsApplication {
 
 	public static void main(final String[] args) {
